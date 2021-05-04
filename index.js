090e02ca86aced0d04ba7a33286fa85d31c8e387
Tiwatope <oniogokristi@gmail.com>
2021-05-04 20:35:42 +0100

    second commit

--- index.js
+++ index.js
@@ -3,13 +3,34 @@ var beforeShare = document.getElementById("beforeShare")
 var before = document.getElementById("before")
 var after = document.getElementById("after")
 
+if ((before.style.display=="") && (after.style.display == "")){
+    before.style.display="grid"
+    after.style.display="none"
+}
+
+
 beforeShare.addEventListener("click",function(){
-    before.style.display="none";
-    after.style.display="grid";
-    console.log(before.style.display)
-    console.log(after.style.display)
+    if ((window.innerWidth >= 1200) && (window.innerWidth > window.innerHeight)){
+        if (after.style.display=="none"){
+            before.style.display="grid"
+            after.style.display="grid"
+            console.log("1 after is displayed")
+        }
+        else {
+            before.style.display="grid"
+            after.style.display="none"
+            console.log("2 after is hidden")
+        }
+    }
+    else {
+        before.style.display="none";
+        after.style.display="grid";
+    }
 })
+
+
 afterShare.addEventListener("click", function(){
     after.style.display="none";
     before.style.display="grid";
 })
+
