64bf94f3942a6d4272cf2b025cbf92ce72cef722
Tiwatope <oniogokristi@gmail.com>
2021-05-04 21:08:22 +0100

    third commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var afterShare = document.getElementById("afterShare")
 var beforeShare = document.getElementById("beforeShare")
 var before = document.getElementById("before")
 var after = document.getElementById("after")
+var Icon = document.getElementById("iShare")
 
 if ((before.style.display=="") && (after.style.display == "")){
     before.style.display="grid"
@@ -14,12 +15,17 @@ beforeShare.addEventListener("click",function(){
         if (after.style.display=="none"){
             before.style.display="grid"
             after.style.display="grid"
+            Icon.style.backgroundColor="hsl(214, 17%, 51%)"
+            Icon.style.color="hsl(210, 46%, 95%)"
             console.log("1 after is displayed")
+            console.log(Icon.style.backgroundColor)
         }
         else {
             before.style.display="grid"
             after.style.display="none"
             console.log("2 after is hidden")
+            Icon.style.backgroundColor="hsl(210, 46%, 95%)"
+            Icon.style.color="hsl(214, 17%, 51%)"
         }
     }
     else {
